57b201c18abf4a1957c4b5672ab9cebe0cc63c36
Brandt <brandtnet1@yahoo.com>
2018-11-15 21:08:36 -0500

    Directories generated successfully for both types

--- FullStackAppGenerator/src/Interface.java
+++ FullStackAppGenerator/src/Interface.java
@@ -1,33 +1,48 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Interface {
 
 	public static void main(String[] args) {
-		try{
 
-			// ProjectGenerator pg = new ProjectGenerator("Python", "Flask", "SQLAlchemy");
+		Scanner scan = new Scanner(System.in);
+		// String frontEnd = "";
+		// String backEnd = "";
+		// String database = "";
+
+		System.out.println("Here we will be getting types and requirements from user.\n");
+
+		// System.out.println("Would you like a Python or ExpressJS front end: ");
+		// frontEnd = scan.nextLine();
+		//
+		// System.out.println("Would you like a NodeJS or FlaskJS back end: ");
+		// backEnd = scan.nextLine();
+		//
+		// System.out.println("Would you like a Python or ExpressJS database: ");
+		// database = scan.nextLine();
+
+		try{
+			// ProjectGenerator pg = new ProjectGenerator(frontEnd, backEnd, database);
 			ProjectGenerator pg = new ProjectGenerator("ExpressJS", "NodeJS", "MongoDB");
 
 			pg.generate();
 		} catch(IOException ex){
 			System.out.println(ex);
+
 		}
 
-
-		System.out.println("Here we will be getting types and requirements from user.");
-
-		System.out.println("Once we get requirements we will create a 'project' object "
-				+ "which will stand in as our intermin full stack appliction");
-
-		System.out.println("The list of requirements and the project object will be passed "
-				+ "into a 'compiler' or sorts which will create the routes, pages and necessary logic");
-
-		System.out.println("A database will be created and instantiated with the necessary "
-				+ "tables and it will be added to the project file");
-
-		System.out.println("Project will be tested 'WORK IN PROGRESS'");
-
-		System.out.println("Project will be spit out into your github");
+		// System.out.println("Once we get requirements we will create a 'project' object "
+		// 		+ "which will stand in as our intermin full stack appliction");
+		//
+		// System.out.println("The list of requirements and the project object will be passed "
+		// 		+ "into a 'compiler' or sorts which will create the routes, pages and necessary logic");
+		//
+		// System.out.println("A database will be created and instantiated with the necessary "
+		// 		+ "tables and it will be added to the project file");
+		//
+		// System.out.println("Project will be tested 'WORK IN PROGRESS'");
+		//
+		// System.out.println("Project will be spit out into your github");
 
 	}
 
--- FullStackAppGenerator/src/ProjectGenerator.java
+++ FullStackAppGenerator/src/ProjectGenerator.java
@@ -27,23 +27,49 @@ public class ProjectGenerator {
 
 	protected void generate() throws IOException {
 
-		if(frontEnd == "ExpressJS") {
+		if(frontEnd.contains("Express")) {
 
-			pb = new ProcessBuilder("express.cmd","app");
+			pb = new ProcessBuilder("express.cmd","webapp");
 			pb.directory(new File("../../"));
 		 	p = pb.start();
 
+			try
+			{
+			    Thread.sleep(1000);
+			}
+			catch(InterruptedException ex)
+			{
+			    Thread.currentThread().interrupt();
+			}
+
 			pb = new ProcessBuilder("npm.cmd","install");
-			pb.directory(new File("../../"));
+			pb.directory(new File("../../webapp"));
 		 	p = pb.start();
 
 		} else {
 
-			File app = new File("../../app");
+			File webapp = new File("../../webapp");
+			webapp.mkdir();
+
+			File config = new File("../../webapp/config.py");
+			config.createNewFile();
+			File site = new File("../../webapp/site.py");
+			site.createNewFile();
+			File app = new File("../../webapp/app");
 			app.mkdir();
+
+			File init = new File("../../webapp/app/__init__.py");
+			init.createNewFile();
+			File forms = new File("../../webapp/app/forms.py");
+			forms.createNewFile();
+			File models = new File("../../webapp/app/models.py");
+			models.createNewFile();
+			File routes = new File("../../webapp/app/routes.py");
+			routes.createNewFile();
+			File templates = new File("../../webapp/templates");
+			templates.mkdir();
 
 		}
 
 	}
-
 }
