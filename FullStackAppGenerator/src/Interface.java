75cd889ba27b73bea1f1d8e2bc69d521d864d24d
Brandt <brandtnet1@yahoo.com>
2018-11-15 19:35:02 -0500

    Express Web-App base generation complete

--- FullStackAppGenerator/src/Interface.java
+++ FullStackAppGenerator/src/Interface.java
@@ -1,23 +1,34 @@
+import java.io.IOException;
 
 public class Interface {
 
 	public static void main(String[] args) {
-		
+		try{
+
+			// ProjectGenerator pg = new ProjectGenerator("Python", "Flask", "SQLAlchemy");
+			ProjectGenerator pg = new ProjectGenerator("ExpressJS", "NodeJS", "MongoDB");
+
+			pg.generate();
+		} catch(IOException ex){
+			System.out.println(ex);
+		}
+
+
 		System.out.println("Here we will be getting types and requirements from user.");
-		
+
 		System.out.println("Once we get requirements we will create a 'project' object "
 				+ "which will stand in as our intermin full stack appliction");
-		
+
 		System.out.println("The list of requirements and the project object will be passed "
 				+ "into a 'compiler' or sorts which will create the routes, pages and necessary logic");
-		
+
 		System.out.println("A database will be created and instantiated with the necessary "
 				+ "tables and it will be added to the project file");
 
 		System.out.println("Project will be tested 'WORK IN PROGRESS'");
-		
+
 		System.out.println("Project will be spit out into your github");
-		
+
 	}
-	
+
 }
--- FullStackAppGenerator/src/ProjectGenerator.java
+++ FullStackAppGenerator/src/ProjectGenerator.java
@@ -1,26 +1,49 @@
+import java.io.IOException;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.lang.ProcessBuilder;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 
 public class ProjectGenerator {
-	
+
 	private String frontEnd;
 	private String backEnd;
 	private String database;
-	
-	private List<String> commands = new ArrayList<String>();                
-    private ProcessBuilder pb;
-	
+
+	private List<String> commands = new ArrayList<String>();
+  private ProcessBuilder pb;
+	private Process p;
+
 	public ProjectGenerator(String frontEnd, String backEnd, String database) {
-		
+
 		this.frontEnd = frontEnd;
 		this.backEnd = backEnd;
 		this.database = database;
-		
+
 	}
-	
-	protected void generateProject() {
-		
+
+	protected void generate() throws IOException {
+
+		if(frontEnd == "ExpressJS") {
+
+			pb = new ProcessBuilder("express.cmd","app");
+			pb.directory(new File("../../"));
+		 	p = pb.start();
+
+			pb = new ProcessBuilder("npm.cmd","install");
+			pb.directory(new File("../../"));
+		 	p = pb.start();
+
+		} else {
+
+			File app = new File("../../app");
+			app.mkdir();
+
+		}
+
 	}
 
 }
